54a3ad2c2b76ce70503bc53031a0edc11324b52b
Justin <justws95@gmail.com>
2020-01-19 12:40:24 -0500

    Added -O2 to build script

--- game/build.sh
+++ game/build.sh
@@ -1,3 +1,3 @@
 swig -c++ -python game.i
-g++ -c -fpic game_wrap.cxx ./src/cell.cpp ./src/game.cpp -I./include -I/usr/include/python3.7
+g++ -c -O2 -fpic game_wrap.cxx ./src/cell.cpp ./src/game.cpp -I./include -I/usr/include/python3.7
 g++ -shared cell.o game.o game_wrap.o -o _game.so
